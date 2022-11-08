c8307269e432972d563764fd983ecd7a9356e0ee
Adriano <adriano_alves_2008@hotmail.com>
2022-11-08 19:12:47 -0300

    Criando a logo

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,11 @@
 import './error.css'
 import { Link } from 'react-router-dom'
+import { Logo } from '../../components/Logo'
 
 function Error() {
   return (
     <div className="error">
+      <Logo />
       <h1>Página não encontrada</h1>
       <p>Essa página não existe</p>
 
