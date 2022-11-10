b762f7a5242cfda4b7587ec8a16620d32d5b0e20
Adriano <adriano_alves_2008@hotmail.com>
2022-11-10 09:27:01 -0300

    useEffect da página private

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -9,6 +9,28 @@ function Private({ children }) {
   const [loading, setLoading] = useState(true)
   const [signed, setSigned] = useState(false)
 
+  useEffect(() => {
+    async function checkLogin() {
+      const unsub = onAuthStateChanged(auth, user => {
+        if (user) {
+          const userData = {
+            uid: user.uid,
+            email: user.email
+          }
+
+          localStorage.setItem('@detailUser', JSON.stringify(userData))
+          setLoading(false)
+          setSigned(true)
+        } else {
+          setLoading(false)
+          setSigned(false)
+        }
+      })
+    }
+
+    checkLogin()
+  }, [])
+
   if (loading) {
     return <div></div>
   }
