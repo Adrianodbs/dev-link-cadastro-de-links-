056c24dda3ed9f64860cb9e44bfd28e3338bf07b
Adriano <adriano_alves_2008@hotmail.com>
2022-11-10 10:26:49 -0300

    css do admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,11 +1,52 @@
 import Header from '../../components/Header'
 import './admin.css'
+import { Logo } from '../../components/Logo'
+import Input from '../../components/Input'
+
+import { MdAddLink } from 'react-icons/md'
+import { FiTrash2 } from 'react-icons/fi'
 
 function Admin() {
   return (
     <div className="admin-container">
       <Header />
-      <h1>Admin</h1>
+      <Logo />
+
+      <form className="form">
+        <label className="label">Nome do link</label>
+        <Input placeholder="Nome do link" />
+
+        <label className="label">Url do link</label>
+        <Input placeholder="Digite a URL" type="url" />
+
+        <section className="container-colors">
+          <div>
+            <label className="label right">Fundo do link</label>
+            <input type="color" />
+          </div>
+          <div>
+            <label className="label right">Cor do link</label>
+            <input type="color" />
+          </div>
+        </section>
+
+        <button className="btn-register" type="submit">
+          Cadastrar <MdAddLink size={24} color="#fff" />
+        </button>
+      </form>
+
+      <h2 className="title">Meus Links</h2>
+      <article
+        className="list animate-pop"
+        style={{ backgroundColor: '#000', color: '#fff' }}
+      >
+        <p>Grupo exclusivo no telegram</p>
+        <div>
+          <button className="btn-delete">
+            <FiTrash2 size={18} color="#fff" />
+          </button>
+        </div>
+      </article>
     </div>
   )
 }
