27f19f8db104b726392360438bda5c6ac85a9ae5
Adriano <adriano_alves_2008@hotmail.com>
2022-11-11 15:53:04 -0300

    adicionando link das redes sociais ao firebase

--- src/pages/Networks/index.js
+++ src/pages/Networks/index.js
@@ -3,20 +3,52 @@ import Header from '../../components/Header'
 import Input from '../../components/Input'
 
 import { MdAddLink } from 'react-icons/md'
+import { useState } from 'react'
+
+import { db } from '../../services/firebaseconnection'
+import { setDoc, doc, getDoc } from 'firebase/firestore'
 
 function Networks() {
+  const [facebook, setFacebook] = useState('')
+  const [instagram, setInstagram] = useState('')
+  const [youtube, setYoutube] = useState('')
+
+  function handleSave(e) {
+    e.preventDefault()
+
+    setDoc(doc(db, 'social', 'link'), {
+      facebook: facebook,
+      instagram: instagram,
+      youtube: youtube
+    })
+      .then(() => {})
+      .catch(() => {})
+  }
+
   return (
     <div className="admin-container">
       <Header />
       <h1 className="title-social">Suas redes sociais</h1>
 
-      <form className="form">
+      <form className="form" onSubmit={handleSave}>
         <label className="label">Link do facebook</label>
-        <Input placeholder="Digite a Url do facebook." />
+        <Input
+          placeholder="Digite a Url do facebook."
+          value={facebook}
+          onChange={e => setFacebook(e.target.value)}
+        />
         <label className="label">Link do instagram</label>
-        <Input placeholder="Digite a Url do instagram." />
+        <Input
+          placeholder="Digite a Url do instagram."
+          value={instagram}
+          onChange={e => setInstagram(e.target.value)}
+        />
         <label className="label">Link do youtube</label>
-        <Input placeholder="Digite a Url do youtube." />
+        <Input
+          placeholder="Digite a Url do youtube."
+          value={youtube}
+          onChange={e => setYoutube(e.target.value)}
+        />
 
         <button type="submit" className="btn-register">
           Salvar links <MdAddLink size={24} color="#fff" />
